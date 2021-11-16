3c143fc586a90de0e7f7910d69a5d9106d5b0e20
Carlo Sala <carlosalag@protonmail.com>
2021-11-16 10:44:49 +0100

    fix: adding 404 page and removing redirects
    redirects caused that robots.txt could not be found

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Center, Heading, Link } from "@chakra-ui/react"
+import NextLink from "next/link"
+
+const Custom404 = () => (
+  <Center flexDir="column" mt="24">
+    <Heading pb="4">This page cannot be found (aka 404 error)</Heading>
+    <NextLink href="/" passHref>
+      <Link>
+        <Heading>HOME PAGE</Heading>
+      </Link>
+    </NextLink>
+  </Center>
+)
+
+export default Custom404
