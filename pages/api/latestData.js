2e9999f71e1affa4206181b107b90e20a891f5fd
Carlo Sala <carlosalag@protonmail.com>
2021-11-16 17:19:50 +0100

    feat: adding cors to api

--- pages/api/latestData.js
+++ pages/api/latestData.js
@@ -1,5 +1,22 @@
+import Cors from "cors"
+
+function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result)
+        }
+        return resolve(result)
+      })
+    })
+}
+
+const cors = initMiddleware(Cors({ methods: ["GET"] }))
+
 const latestData = async (req, res) => {
   const url = `https://freecurrencyapi.net/api/v2/latest?apikey=${process.env.APIKEY}`
+  await cors(req, res)
   try {
     const { data } = await fetch(url).then((r) => r.json())
     res.status(200).json(data)
