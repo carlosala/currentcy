a85d7f1b2056468d0b6488f6cc365f152255af12
Carlo Sala <carlosalag@protonmail.com>
2022-01-28 18:38:09 +0100

    chore: change type from Object to Record in JSDoc

--- hooks/useData.js
+++ hooks/useData.js
@@ -4,14 +4,14 @@ import useSWR from "swr"
  * Fetcher.
  *
  * @param {string} url
- * @returns {Promise<Object<string, number>>}
+ * @returns {Promise<Record<string, number>>}
  */
 const fetcher = (url) => fetch(url).then((r) => r.json())
 
 /**
  * UseData.
  *
- * @returns {Object<string, number>}
+ * @returns {Record<string, number>}
  */
 export default function useData() {
   const url = "https://currentcy.carlosala.dev/api/latestdata"
