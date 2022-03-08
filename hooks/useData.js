33beeb3a5dec8bd0c0b15a63f2d1ab0b83642b88
Carlo Sala <carlosalag@protonmail.com>
2022-03-08 08:48:47 +0100

    fix: currencyapi type changes

--- hooks/useData.js
+++ hooks/useData.js
@@ -1,20 +1,23 @@
 import useSWR from "swr"
 
+/** @typedef {{ code: string; value: number }} Currency */
+
 /**
  * Fetcher.
  *
  * @param {string} url
- * @returns {Promise<Record<string, number>>}
+ * @returns {Promise<Record<string, Currency>>}
  */
 const fetcher = (url) => fetch(url).then((r) => r.json())
 
 /**
  * UseData.
  *
- * @returns {Record<string, number>}
+ * @returns {Record<string, Currency>}
  */
 export default function useData() {
   const url = "https://currentcy.carlosala.dev/api/latestdata"
   const { data } = useSWR(url, fetcher)
+  if (data) console.log(data)
   return data
 }
