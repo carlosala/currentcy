8d0700cad31a2849d672b7fa57c3e9d7569073d2
Carlo Sala <carlosalag@protonmail.com>
2022-06-23 09:03:06 +0200

    refactor: document now is a functional component

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,15 @@
 import { ColorModeScript } from "@chakra-ui/react"
-import Document, { Head, Html, Main, NextScript } from "next/document"
+import { Head, Html, Main, NextScript } from "next/document"
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <ColorModeScript />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <ColorModeScript />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument
