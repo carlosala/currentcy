24d2af221a03e91c28bba7f0e62b055e2f5c192c
Carlo Sala <carlosalag@protonmail.com>
2023-06-01 16:20:37 +0200

    feat: add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,30 @@
 import { ChakraProvider } from "@chakra-ui/react"
+import { Analytics } from "@vercel/analytics/react"
 import Head from "next/head"
 import theme from "styles/theme"
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Head>
-        <title>Currentcy</title>
-        <link
-          rel="preload"
-          href="https://currentcy.carlosala.dev/api/latestdata"
-          as="fetch"
-          crossOrigin="anonymous"
-        />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content="Simple real-time currency converter"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <Head>
+          <title>Currentcy</title>
+          <link
+            rel="preload"
+            href="https://currentcy.carlosala.dev/api/latestdata"
+            as="fetch"
+            crossOrigin="anonymous"
+          />
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="description"
+            content="Simple real-time currency converter"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <Analytics />
+    </>
   )
 }
